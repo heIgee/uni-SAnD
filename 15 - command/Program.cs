343d10558ea1968c838a6e1f0595a8b6995640ca
heIgee <oleo7979@gmail.com>
2024-11-24 15:24:33 +0200

    TMP

---
+++ 15 - command/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("C# Command pattern");
+
+var receiver = new Receiver();
+var command = new RealCommand(receiver, new("[crucial data]"));
+var invoker = new Invoker(command);
+invoker.ExecuteCommand();
+
+
+record Args(
+  string Data
+);
+
+interface ICommand
+{
+  void Execute();
+}
+
+class RealCommand(Receiver receiver, Args args) : ICommand
+{
+  public void Execute()
+  {
+    receiver.BusinessLogic(args);
+  }
+}
+
+class Receiver
+{
+  public void BusinessLogic(Args args)
+  {
+    Console.WriteLine($"Evaluating {args}");
+  }
+}
+
+class Invoker(ICommand command)
+{
+  public ICommand Command { get; set; } = command;
+
+  public void ExecuteCommand()
+  {
+    Command.Execute();
+  }
+}
---
+++ 15 - command/index.ts
@@ -0,0 +1,1 @@
+console.log('TypeScript Command pattern');
